9b60757abf658c67ae8093442873dd621886027c
Jucemar Dimon <jucemar.dimon@gmail.com>
2020-10-26 21:55:57 -0300

    Create the repository

---
+++ src/pages/Home/index.js
@@ -0,0 +1,109 @@
+import React, { Fragment, useEffect, useState } from "react";
+import { Helmet } from "react-helmet";
+import Logo from "../../assets/images/coin.png";
+import "rbx/index.css";
+import {
+  Container,
+  Title,
+  Navbar,
+  Footer,
+  Content,
+  Column,
+  Box,
+  Section,
+} from "rbx";
+
+// import { Container } from './styles';
+
+const Home = () => {
+  const [coins, setCoins] = useState([]);
+  useEffect(() => {
+    fetch(
+      "https://pro-api.coinmarketcap.com/v1/cryptocurrency/map?CMC_PRO_API_KEY=" +
+        process.env.REACT_APP_APY_KEY_COIN_MARKET_CAP
+    )
+      .then((response) => {
+        if (!response.ok)
+          throw new Error(
+            "Erro ao executar a requisição, status " + response.status
+          );
+        return response.json();
+      })
+      .then((api) => {
+        console.log(api.data);
+        setCoins(Object.values(api.data.slice(0, 100)));
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+  return (
+    <Fragment>
+      <Helmet>
+        <title>Cryptocurrencies</title>
+      </Helmet>
+      <Navbar color="light">
+        <Container>
+          <Navbar.Brand>
+            <Navbar.Item href="#">
+              <img src={Logo} alt="" role="presentation" height="50" />
+            </Navbar.Item>
+            <Navbar.Burger />
+          </Navbar.Brand>
+          <Navbar.Menu>
+            <Navbar.Segment align="start">
+              <Navbar.Item>Home</Navbar.Item>
+              <Navbar.Item>Documentation</Navbar.Item>
+            </Navbar.Segment>
+          </Navbar.Menu>
+        </Container>
+      </Navbar>
+
+      <Section>
+        <Container>
+          <Column.Group multiline centered breakpoint="mobile">
+            {coins.map((coin) => (
+              <Column
+                narrow
+                key={`COIN-${coin.name}`}
+                style={{ minWidth: 160 }}
+              >
+                <Box centered>
+                  <Title as="p" size={5}>
+                    {coin.name}
+                  </Title>
+                  <Title as="p" subtitle size={6}>
+                    {coin.symbol}
+                  </Title>
+                  <Title as="p" subtitle size={6}>
+                    {coin.rank}
+                  </Title>
+                </Box>
+              </Column>
+            ))}
+          </Column.Group>
+        </Container>
+      </Section>
+
+      <Footer>
+        <Content textAlign="centered">
+          <p>
+            <strong>rbx</strong> by{" "}
+            <a href="https://github.com/dfee">Devin Fee</a>. The source code is
+            released under the{" "}
+            <a href="https://opensource.org/licenses/mit-license.php">
+              MIT License
+            </a>
+            . The website content is licensed{" "}
+            <a href="https://creativecommons.org/licenses/by-nc-sa/4.0/">
+              CC BY NC SA 4.0
+            </a>
+            .
+          </p>
+        </Content>
+      </Footer>
+    </Fragment>
+  );
+};
+
+export default Home;
